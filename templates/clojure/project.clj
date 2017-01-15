ed05d8d84f1ddadd1b505a91f7128912cc655c3d
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-15 16:32:38 +0100

    clojure deps

--- templates/clojure/project.clj
+++ templates/clojure/project.clj
@@ -3,7 +3,12 @@
   :dependencies
     [[org.clojure/clojure "1.8.0"]
      [http-kit "2.2.0"]
-     [compojure "1.5.1"]]
+     [compojure "1.5.1"]
+     [org.clojure/core.match "0.3.0-alpha4"]
+     [org.clojure/core.incubator "0.1.4"]]
   :main <%= name %>
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:source-paths ["test"]
+                   :dependencies [[midje "1.8.3"]
+                                  [proto-repl "0.3.1"]]}})
